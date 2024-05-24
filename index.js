6714cbd5b907e25a92a287ed51979d5a431a306f
Kusack01 <127121825+Kusack01@users.noreply.github.com>
2024-05-24 12:37:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const port = 3001;
+
+const { getRobots , getRobotById } = require("./controllers/robots.controller");
+
+//-------------------------------------
+
+
+app.get("/", getRobots);
+
+app.get("/:id" , getRobotById)
+
+
+
+//-------------------------------------
+
+app.listen(port,()=> {    
+    console.log(`Conectado correctamenta al puerto ${port}`);
+    console.log(`Puerto ${port} disponible`);
+});
+
+//npm run start -> lo corre atrvez del script
+//clg ingresa console.log automaticamente
+//, , se llama middlewhere
